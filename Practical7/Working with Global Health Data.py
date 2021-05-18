db4829aa40cf79f10a8b631269ee40226161732a
yuganyugan <1450177262@qq.com>
2021-05-18 17:42:37 +0100

    Global health Data

---
+++ Practical7/Working with Global Health Data.py
@@ -0,0 +1,8 @@
+# input two lists
+listA=[9410,394141,4442,105338,19149,76779,126550,36296,842,15981]
+listB=[51,1142,42,216,25,650,32533,57,1,523]
+
+# appending the elements of listB into listA
+import numpy as np
+average =np.array(listA)/np.array(listB)
+print(average)
